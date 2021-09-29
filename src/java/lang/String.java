9e7464c499941e555701d4677c37f5fc2fc34911
zcl <820861340@qq.com>
2021-09-29 15:59:15 +0800

    双亲委派机制

---
+++ src/java/lang/String.java
@@ -0,0 +1,12 @@
+package java.lang;
+
+/**
+ * @author zcl
+ * @date 2021/9/29 15:44
+ */
+/*public class String {
+    static {
+        System.out.println("String class's static code block");
+    }
+
+}*/
---
+++ src/jvm/StringTest.java
@@ -0,0 +1,13 @@
+package jvm;
+import java.lang.String;
+
+/**
+ * @author zcl
+ * @date 2021/9/29 15:42
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        String string = new String();
+        System.out.println("Hello,cute coder");
+    }
+}
