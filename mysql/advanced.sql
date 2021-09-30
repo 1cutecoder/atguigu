b9b7c895c0f71edff386437c7cd2f31603fa8c35
zcl <820861340@qq.com>
2021-09-30 09:56:08 +0800

    1.doc mysql sqls

---
+++ mysql/advanced.sql
@@ -0,0 +1,73 @@
+CREATE TABLE `tbl_dept`(
+`id` INT(11) not NULL auto_increment,
+`deptName` VARCHAR(30) DEFAULT NULL,
+`idAdd` VARCHAR(40) DEFAULT NULL,
+PRIMARY KEY (`id`)
+
+) ENGINE=INNODB auto_increment=1 DEFAULT CHARSET=utf8;
+CREATE TABLE `tbl_emp`(
+`id` INT(11) not NULL auto_increment,
+`name` VARCHAR(20) DEFAULT NULL,
+`deptId` INT(11) DEFAULT NULL,
+PRIMARY KEY (`id`),
+KEY `fk_dept_id` (`deptId`)
+#CONSTRAINT `fk_dept_id` foreign key (`deptId`) REFERENCES `tbl.dept` (id)
+) ENGINE=INNODB auto_increment=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO tbl_dept(deptName,idAdd) VALUES ('RD',11);
+INSERT INTO tbl_dept(deptName,idAdd) VALUES ('HR',12);
+INSERT INTO tbl_dept(deptName,idAdd) VALUES ('MK',13);
+INSERT INTO tbl_dept(deptName,idAdd) VALUES ('MIS',14);
+INSERT INTO tbl_dept(deptName,idAdd) VALUES ('FD',15);
+ 
+INSERT INTO tbl_emp(NAME,deptId) VALUES ('z3',1);
+INSERT INTO tbl_emp(NAME,deptId) VALUES ('z4',1);
+INSERT INTO tbl_emp(NAME,deptId) VALUES ('z5',1);
+INSERT INTO tbl_emp(NAME,deptId) VALUES ('w5',2);
+INSERT INTO tbl_emp(NAME,deptId) VALUES ('w6',2);
+INSERT INTO tbl_emp(NAME,deptId) VALUES ('s7',3);
+INSERT INTO tbl_emp(NAME,deptId) VALUES ('s8',4);
+INSERT INTO tbl_emp(NAME,deptId) VALUES ('s9',51);
+SELECT * from tbl_dept
+SELECT * from tbl_emp a INNER JOIN tbl_dept b on a.deptid = b.id
+union
+SELECT * from tbl_emp a left JOIN tbl_dept b on a.deptid = b.id where b.id is null
+union
+SELECT * from tbl_emp a right JOIN tbl_dept b on a.deptid = b.id where a.id is null
+
+explain SELECT * from tbl_emp a  JOIN tbl_dept b 
+	on a.deptid = b.id；
+#show index from tbl_emp
+--
+explain SELECT * from tbl_emp where deptId =1
+#mysql优化
+#建表
+ CREATE TABLE t1(id INT(10) AUTO_INCREMENT,content  VARCHAR(100) NULL ,  PRIMARY KEY (id)); 
+ CREATE TABLE t2(id INT(10) AUTO_INCREMENT,content  VARCHAR(100) NULL ,  PRIMARY KEY (id)); 
+ CREATE TABLE t3(id INT(10) AUTO_INCREMENT,content  VARCHAR(100) NULL ,  PRIMARY KEY (id));
+ CREATE TABLE t4(id INT(10) AUTO_INCREMENT,content  VARCHAR(100) NULL ,  PRIMARY KEY (id));  
+ INSERT INTO t1(content) VALUES(CONCAT('t1_',FLOOR(1+RAND()*1000)));  
+ INSERT INTO t2(content) VALUES(CONCAT('t2_',FLOOR(1+RAND()*1000)));   
+ INSERT INTO t3(content) VALUES(CONCAT('t3_',FLOOR(1+RAND()*1000)));      
+ INSERT INTO t4(content) VALUES(CONCAT('t4_',FLOOR(1+RAND()*1000)));
+#id相同，执行顺序由上至下
+EXPLAIN select t2.* from t1,t2,t3
+where t1.id = t2.id and t1.id = t3.id
+and t1.content ='';
+
+#id不同，如果是子查询，id的序号会递增，id值越大优先级越高，越先被执行
+EXPLAIN select t2.* from t2 where id =(
+	select id from t2 where id = (
+		select t3.id from t3 where t3.content='')
+	);
+	
+	#
+explain select t2.* from 
+(select t3.id from t3 where t3.content = '') s1, t2
+where s1.id = t2.id;
+#union
+explain
+SELECT * from tbl_emp a left JOIN tbl_dept b on a.deptid = b.id where b.id is null
+union
+SELECT * from tbl_emp a right JOIN tbl_dept b on a.deptid = b.id where a.id is null
+#derived
