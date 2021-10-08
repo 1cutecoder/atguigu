ba30f0bc4b3350ce14c93233d0ef08949968e5c1
zcl <820861340@qq.com>
2021-10-08 18:05:48 +0800

    1.doc pcregister

---
+++ src/jvm/PCRegisterTest.java
@@ -0,0 +1,13 @@
+package jvm;
+
+/**
+ * @author zcl
+ * @date 2021/10/8 17:56
+ */
+public class PCRegisterTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+    }
+}
