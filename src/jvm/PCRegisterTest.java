c2d2630013a50a3933647841b8bfe0c71d401f24
zcl <820861340@qq.com>
2021-10-13 09:53:47 +0800

    1.doc pcregister

--- src/jvm/PCRegisterTest.java
+++ src/jvm/PCRegisterTest.java
@@ -9,5 +9,8 @@ public class PCRegisterTest {
         int i = 10;
         int j = 20;
         int k = i + j;
+        String s = "abc";
+        System.out.println(i);
+        System.out.println(k);
     }
 }
