00500b77f137f1507fd51141c93c7da3a3478486
zcl <820861340@qq.com>
2021-10-27 14:14:44 +0800

    1.ZeroEvenOdd

--- src/leetcode/ZeroEvenOdd.java
+++ src/leetcode/ZeroEvenOdd.java
@@ -9,9 +9,9 @@ import java.util.function.IntConsumer;
  */
 class ZeroEvenOdd {
     private int n;
-    private volatile int i = 0;
     Semaphore zeroSema = new Semaphore(1);
-    Semaphore evenOddSema = new Semaphore(0);
+    Semaphore evenSema = new Semaphore(0);
+    Semaphore oddSema = new Semaphore(0);
 
     public ZeroEvenOdd(int n) {
         this.n = n;
@@ -19,27 +19,38 @@ class ZeroEvenOdd {
 
     // printNumber.accept(x) outputs "x", where x is an integer.
     public void zero(IntConsumer printNumber) throws InterruptedException {
-        while (i < n) {
+        for (int i = 1; i <= n; i++) {
             zeroSema.acquire();
             printNumber.accept(0);
-            i++;
-            evenOddSema.release();
+            if (i % 2 == 0) {
+                evenSema.release();
+            } else {
+                oddSema.release();
+            }
         }
     }
 
     public void even(IntConsumer printNumber) throws InterruptedException {
-        evenOddSema.acquire();
-        if (i % 2 == 0 && i < n) {
-            printNumber.accept(i);
-            zeroSema.release();
+        for (int i = 1; i <= n; i++) {
+            //偶数 打印偶数 并释放zero的线程
+            if (i % 2 == 0) {
+                evenSema.acquire();
+                printNumber.accept(i);
+                zeroSema.release();
+            }
         }
+
     }
 
     public void odd(IntConsumer printNumber) throws InterruptedException {
-        evenOddSema.acquire();
-        if (i % 2 == 1 && i < n) {
-            printNumber.accept(i);
-            zeroSema.release();
+        for (int i = 1; i <= n; i++) {
+            //奇数，打印奇数，并释放zero的线程
+            if (i % 2 == 1) {
+                oddSema.acquire();
+                printNumber.accept(i);
+                zeroSema.release();
+            }
         }
+
     }
 }
