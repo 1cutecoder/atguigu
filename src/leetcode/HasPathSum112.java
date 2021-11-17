ff880f53d17012518b981bdee8e973aed119e39d
zcl <820861340@qq.com>
2021-11-17 18:05:53 +0800

    1.HasPathSum112

---
+++ src/leetcode/HasPathSum112.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * @author zcl
+ * @date 2021/11/17 18:05
+ */
+public class HasPathSum112 {
+    class Solution {
+        public boolean hasPathSum(TreeNode root, int targetSum) {
+            if( root.left ==null && root.right == null) {
+                return targetSum==0;
+            }
+            return hasPathSum(root.left,targetSum - root.val) || hasPathSum(root.right,targetSum - root.val);
+        }
+    }
+}
