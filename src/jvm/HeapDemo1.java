3c4d2a65a2841cf30e875ecf856c3f09fe96d2a3
zcl <820861340@qq.com>
2021-11-25 17:36:26 +0800

    1.jvm 堆空间分配

---
+++ src/jvm/HeapDemo1.java
@@ -0,0 +1,16 @@
+package jvm;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zcl
+ * @date 2021/11/25 17:34
+ */
+public class HeapDemo1 {
+    public static void main(String[] args) {
+        System.out.println("start***********");
+        try {
+            TimeUnit.SECONDS.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+        System.out.println("end***************");
+    }
+}
