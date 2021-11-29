48a551e6ebe24090d0919b0c514c76a77e5ba186
zcl <820861340@qq.com>
2021-11-29 15:43:55 +0800

    1.开闭原则

---
+++ src/designpattern/Demo.java
@@ -0,0 +1,11 @@
+package designpattern;
+
+/**
+ * @author zcl
+ * @date 2021/11/29 14:07
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+    }
+}
