bbe766ab1958fb50bdad4f6ef20239c1c279541f
zcl <820861340@qq.com>
2021-11-29 16:20:48 +0800

    1.里氏代换原则 after

---
+++ src/designpattern/principles/demo2/after/Quadrilateral.java
@@ -0,0 +1,11 @@
+package designpattern.principles.demo2.after;
+
+/**
+ * @author zcl
+ * @date 2021/11/29 15:56
+ */
+public interface Quadrilateral {
+    double getLength();
+
+    double getWidth();
+}
