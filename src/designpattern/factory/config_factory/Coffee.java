11da16773d6cf0e857d4088f0c18c6e78793a70b
zcl <820861340@qq.com>
2021-12-08 14:42:48 +0800

    1.PatternExtension

---
+++ src/designpattern/factory/config_factory/Coffee.java
@@ -0,0 +1,17 @@
+package designpattern.factory.config_factory;
+
+/**
+ * @author zcl
+ * @date 2021/12/7 17:25
+ */
+public abstract class Coffee {
+    public abstract String getName();
+
+
+    public void addSugar(){
+        System.out.println("╪слг");
+    }
+    public void addMilk(){
+        System.out.println("╪сдл");
+    }
+}
