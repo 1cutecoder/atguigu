9c6468cae347cc5b770d6fd0dbbecfb1a57986f7
zcl <820861340@qq.com>
2021-12-10 14:52:01 +0800

    static proxy

---
+++ src/designpattern/proxy/ProxyPoint.java
@@ -0,0 +1,18 @@
+package designpattern.proxy;
+
+/**
+ * @author zcl
+ * @date 2021/12/10 14:23
+ */
+public class ProxyPoint implements SellTickets{
+    private TrainStation trainStation = new TrainStation();
+    /**
+     * 卖火车票
+     */
+    @Override
+    public void sell() {
+        System.out.println("============代收点start===================");
+        trainStation.sell();
+        System.out.println("============代收点end=================");
+    }
+}
---
+++ src/designpattern/proxy/TrainStation.java
@@ -0,0 +1,16 @@
+package designpattern.proxy;
+
+/**
+ * @author zcl
+ * @date 2021/12/10 14:23
+ */
+public class TrainStation implements SellTickets {
+
+    /**
+     * Âô»ğ³µÆ±
+     */
+    @Override
+    public void sell() {
+        System.out.println("»ğ³µÕ¾ÂôÆ±");
+    }
+}
