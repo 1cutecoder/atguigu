32af49dd712fb50136a47ebfea1e1816b1a23a12
zcl <820861340@qq.com>
2021-12-21 15:33:45 +0800

    flyweight pattern

---
+++ src/main/java/designpattern/flyweight/TestString.java
@@ -0,0 +1,20 @@
+package designpattern.flyweight;
+
+/**
+ * @author zcl
+ * @date 2021/12/21 15:30
+ */
+public class TestString {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "abc";
+        String s3 = new String("abc");
+        String s4 =  new String("abc");
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s3 == s4);
+        System.out.println(s3.intern() == s1);
+        System.out.println(s3.intern() == s4.intern());
+
+    }
+}
