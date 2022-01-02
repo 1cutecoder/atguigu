122b6a7ec6a4ab9b716a6ba2d791329f0a1704cd
coder <820861340@qq.com>
2022-01-02 21:44:25 +0800

    分散读取

--- src/test/netty/c1/TestScatteringWrites.java
+++ src/test/netty/c1/TestScatteringWrites.java
@@ -22,13 +22,27 @@ public class TestScatteringWrites {
         ByteBuffer buffer = StandardCharsets.UTF_8.encode("hello");
         ByteBuffer buffer1 = StandardCharsets.UTF_8.encode("world");
         ByteBuffer buffer2 = StandardCharsets.UTF_8.encode("你好");
+        FileChannel channel = null;
         try {
-            FileChannel channel = new RandomAccessFile("words.txt", "rw").getChannel();
+            channel = new RandomAccessFile("words.txt", "rw").getChannel();
             channel.write(new ByteBuffer[]{buffer,buffer1,buffer2});
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            closeResouece(channel);
             e.printStackTrace();
+        } finally {
+            closeResouece(channel);
+        }
+    }
+
+    private static void closeResouece(FileChannel channel) {
+        if (channel != null) {
+            try {
+                channel.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
