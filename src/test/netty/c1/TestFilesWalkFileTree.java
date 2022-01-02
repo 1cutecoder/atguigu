f34dbe8acb532b7b400e0996760089c5c83be580
coder <820861340@qq.com>
2022-01-02 22:19:46 +0800

    删除多级目录

--- src/test/netty/c1/TestFilesWalkFileTree.java
+++ src/test/netty/c1/TestFilesWalkFileTree.java
@@ -14,9 +14,40 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class TestFilesWalkFileTree {
     public static void main(String[] args) throws IOException {
+//        deleteFile();
+    }
+
+    private static void deleteFile() throws IOException {
+        System.out.println(System.getProperty("file.encoding"));
+
         AtomicInteger dirCount = new AtomicInteger();
         AtomicInteger fileCount = new AtomicInteger();
-        Files.walkFileTree(Paths.get("D:\\workspace\\atguigu\\src\\test\\netty\\c1"),new SimpleFileVisitor<Path>(){
+        Files.walkFileTree(Paths.get("D:\\workspace\\atguigu\\src\\test\\netty\\c1\\file"), new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+//                System.out.println("========>进入" + dir);
+                dirCount.incrementAndGet();
+                return super.preVisitDirectory(dir, attrs);
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return super.visitFile(file, attrs);
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+//                System.out.println("<========退出" + dir);
+                return super.postVisitDirectory(dir, exc);
+            }
+        });
+    }
+
+    private static void m2() throws IOException {
+        AtomicInteger dirCount = new AtomicInteger();
+        AtomicInteger fileCount = new AtomicInteger();
+        Files.walkFileTree(Paths.get("D:\\workspace\\atguigu\\src\\test\\netty\\c1"), new SimpleFileVisitor<Path>() {
             @Override
             public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
                 System.out.println("===========" + dir);
@@ -40,7 +71,7 @@ public class TestFilesWalkFileTree {
     private static void m1() throws IOException {
         AtomicInteger dirCount = new AtomicInteger();
         AtomicInteger fileCount = new AtomicInteger();
-        Files.walkFileTree(Paths.get("D:\\workspace\\atguigu\\src\\test\\netty\\c1"),new SimpleFileVisitor<Path>(){
+        Files.walkFileTree(Paths.get("D:\\workspace\\atguigu\\src\\test\\netty\\c1"), new SimpleFileVisitor<Path>() {
             @Override
             public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
                 System.out.println("===========" + dir);
