9310627381d5b6792f26ad12d117bd13e15aaaaf
zcl <820861340@qq.com>
2022-01-04 16:31:31 +0800

    向客户端写数据，阻塞不好-->关注可写事件

--- src/test/netty/c5/WriteServer.java
+++ src/test/netty/c5/WriteServer.java
@@ -5,10 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.nio.charset.Charset;
 import java.util.Iterator;
 
@@ -20,7 +17,7 @@ import java.util.Iterator;
 public class WriteServer {
     public static void main(String[] args) {
         ServerSocketChannel ssc = null;
-        Selector selector =null;
+        Selector selector = null;
         try {
             ssc = ServerSocketChannel.open();
             ssc.configureBlocking(false);
@@ -33,18 +30,33 @@ public class WriteServer {
                 while (iterator.hasNext()) {
                     SelectionKey key = iterator.next();
                     iterator.remove();
+                    //刚建立连接
                     if (key.isAcceptable()) {
                         SocketChannel sc = ssc.accept();
                         sc.configureBlocking(false);
+                        SelectionKey scKey = sc.register(selector, 0, null);
                         StringBuilder sb = new StringBuilder();
                         for (int i = 0; i < 30_000_000; i++) {
                             sb.append("a");
                         }
                         ByteBuffer buffer = Charset.defaultCharset().encode(sb.toString());
-                        while (buffer.hasRemaining()) {
-                            int write = sc.write(buffer);
-                            System.out.println("write = " + write);
-
+                        int write = sc.write(buffer);
+                        System.out.println("write = " + write);
+                        if (buffer.hasRemaining()) {
+                            //关注可写事件
+                            scKey.interestOps(scKey.interestOps() + SelectionKey.OP_WRITE);
+                            //未写完的数据挂到scKey上
+                            scKey.attach(buffer);
+                        }
+                    } else if (key.isWritable()) {
+                        ByteBuffer buffer = (ByteBuffer) key.attachment();
+                        SocketChannel sc = (SocketChannel) key.channel();
+                        int write = sc.write(buffer);
+                        System.out.println("write = " + write);
+                        //清理操作
+                        if (!buffer.hasRemaining()) {
+                            key.attach(null);
+                            key.interestOps(key.interestOps() - SelectionKey.OP_WRITE);
                         }
                     }
                 }
