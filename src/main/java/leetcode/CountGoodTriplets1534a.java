fa91cf293a693521dcf0e51cd1ac786a9ab63055
zcl <820861340@qq.com>
2022-01-06 17:27:52 +0800

    多个handler处理

---
+++ src/main/java/leetcode/CountGoodTriplets1534a.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+/**
+ * @author zcl
+ * @date 2022/1/6 16:05
+ */
+public class CountGoodTriplets1534a {
+    public static void main(String[] args) {
+
+    }
+}
--- src/test/netty/c3/EventLoopServer.java
+++ src/test/netty/c3/EventLoopServer.java
@@ -5,6 +5,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.DefaultEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -19,20 +20,29 @@ import java.nio.charset.Charset;
 @Slf4j
 public class EventLoopServer {
     public static void main(String[] args) {
+        DefaultEventLoopGroup group = new DefaultEventLoopGroup();
         new ServerBootstrap()
-                .group(new NioEventLoopGroup())
+                //细分1：boss只负责ServerSocketChannel上accept事件 worker只负责SocketChannel上的读写
+                .group(new NioEventLoopGroup(),new NioEventLoopGroup(2))
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                        ch.pipeline().addLast("handler1",new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.info(buf.toString(Charset.defaultCharset()));
+                                ctx.fireChannelRead(msg);
+                            }
+                        }).addLast(group,"handler2",new ChannelInboundHandlerAdapter() {
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                                 ByteBuf buf = (ByteBuf) msg;
                                 log.info(buf.toString(Charset.defaultCharset()));
                             }
                         });
-;                    }
+                    };
                 })
                 .bind(8080);
     }
