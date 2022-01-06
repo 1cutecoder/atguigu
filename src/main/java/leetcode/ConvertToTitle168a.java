f32066d209823d27dccb35145db554101444bbaa
zcl <820861340@qq.com>
2022-01-06 16:03:56 +0800

    168.Excel表列名称

---
+++ src/main/java/leetcode/ConvertToTitle168a.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ * @author zcl
+ * @date 2022/1/6 14:58
+ */
+public class ConvertToTitle168a {
+    class Solution {
+        public String convertToTitle(int columnNumber) {
+            StringBuffer sb = new StringBuffer();
+            int r = 0;
+
+            while (columnNumber > 0) {
+                r = columnNumber % 26;
+                if (r == 0) {
+                    sb.append('Z');
+                    columnNumber -= 26;
+                } else {
+                    sb.append((char)('A' +r -1));
+                }
+                columnNumber /= 26;
+            }
+
+            return sb.reverse().toString();
+        }
+    }
+}
