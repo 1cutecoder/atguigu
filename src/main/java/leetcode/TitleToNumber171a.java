4462fd8a8bdb8a17fb13d5ef64db69dffab7a464
zcl <820861340@qq.com>
2022-01-06 15:07:17 +0800

    171. Excel表列序号

---
+++ src/main/java/leetcode/TitleToNumber171a.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * @author zcl
+ * @date 2022/1/6 14:58
+ */
+public class TitleToNumber171a {
+    class Solution {
+        public int titleToNumber(String columnTitle) {
+            int n = 0;
+            int multiple = 1;
+            for (int i = columnTitle.length() -1; i >= 0; i--) {
+                n += (columnTitle.charAt(i) - 'A' + 1) * multiple;
+                multiple *= 26;
+            }
+            return n;
+        }
+    }
+}
