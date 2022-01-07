3cc3c4981c92fc66ab94927a5b81e48927b537ea
zcl <820861340@qq.com>
2022-01-07 15:37:47 +0800

    1.Netty Future addListener

--- src/test/netty/c3/TestNettyFuture.java
+++ src/test/netty/c3/TestNettyFuture.java
@@ -3,6 +3,7 @@ package netty.c3;
 import io.netty.channel.EventLoop;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Callable;
@@ -21,7 +22,7 @@ public class TestNettyFuture {
         Future<Integer> future = eventLoop.submit(new Callable<Integer>() {
             @Override
             public Integer call() throws Exception {
-                log.debug("执行极端");
+                log.debug("执行计算");
                 try {
                     TimeUnit.SECONDS.sleep(1);
                 } catch (InterruptedException e) {
@@ -30,7 +31,13 @@ public class TestNettyFuture {
                 return 70;
             }
         });
-        log.debug("等待结果");
-        log.debug("结果{}", future.get());
+//        log.debug("等待结果");
+//        log.debug("结果{}", future.get());
+        future.addListener(new GenericFutureListener<Future<? super Integer>>() {
+            @Override
+            public void operationComplete(Future<? super Integer> future) throws Exception {
+                log.debug("接收结果:{}",future.getNow());
+            }
+        });
     }
 }
