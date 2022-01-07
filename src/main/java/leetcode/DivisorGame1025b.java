5d3dc43fc57ee4a154bb55dacccb6ece1157f41f
zcl <820861340@qq.com>
2022-01-07 10:39:11 +0800

    1025.除数博弈

---
+++ src/main/java/leetcode/DivisorGame1025b.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * @author zcl
+ * @date 2022/1/7 9:18
+ */
+public class DivisorGame1025b {
+    static class Solution {
+        public boolean divisorGame(int n) {
+            return n % 2 == 0;
+        }
+    }
+
+
+}
