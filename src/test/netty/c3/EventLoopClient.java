97a6ba8e692cd0e91529ddd70fa3db77c1c842fe
zcl <820861340@qq.com>
2022-01-07 14:22:19 +0800

    1、使用sync 方法处理同步结果 2、使用addListener(回调对象) 方法异步处理结果

--- src/test/netty/c3/EventLoopClient.java
+++ src/test/netty/c3/EventLoopClient.java
@@ -3,15 +3,18 @@ package netty.c3;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author zcl
  * @date 2022/1/5 16:34
  */
+@Slf4j
 public class EventLoopClient {
     public static void main(String[] args) throws InterruptedException {
         ChannelFuture channelFuture = new Bootstrap()
@@ -24,10 +27,20 @@ public class EventLoopClient {
                     }
                 })
                 .connect("localhost", 8080);
-      channelFuture.sync();
-      channelFuture.channel();
-        Channel channel = channelFuture.channel();
-        channel.writeAndFlush("hello,world");
+        //1、使用sync 方法处理同步结果
+        /**channelFuture.sync();
+         channelFuture.channel();
+         Channel channel = channelFuture.channel();
+         channel.writeAndFlush("hello,world");*/
+        //2、使用addListener(回调对象) 方法异步处理结果
+        channelFuture.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                Channel channel = channelFuture.channel();
+                log.info("{}", channel);
+                channel.writeAndFlush("hello,world");
+            }
+        });
         System.out.println("");
     }
 }
