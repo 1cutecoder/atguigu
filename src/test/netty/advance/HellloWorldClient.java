fc9d50a0d0a7108b34d1ee73fe6afbe84cd1122e
coder <820861340@qq.com>
2022-01-09 16:46:25 +0800

    粘包半包问题

--- src/test/netty/advance/HellloWorldClient.java
+++ src/test/netty/advance/HellloWorldClient.java
@@ -32,12 +32,12 @@ public class HellloWorldClient {
                 .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
                             @Override
                             public void channelActive(ChannelHandlerContext ctx) throws Exception {
                                 for (int i = 0; i < 10; i++) {
                                     ByteBuf buf = ctx.alloc().buffer();
-                                    buf.writeBytes(new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,31,31,32,33,34,35,36});
+                                    buf.writeBytes(new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 31, 32, 33, 34, 35, 36});
                                     ctx.writeAndFlush(buf);
                                 }
                             }
