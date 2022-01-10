e91e4cb7588c4562432132f2c4425a1b9e2918bf
zcl <820861340@qq.com>
2022-01-10 17:57:51 +0800

    1.chat room

---
+++ src/test/netty/message/Message.java
@@ -0,0 +1,10 @@
+package netty.message;
+
+import java.io.Serializable;
+
+/**
+ * @author zcl
+ * @date 2022/1/10 17:56
+ */
+public class Message implements Serializable {
+}
---
+++ src/test/netty/protocol/MessageCodec.java
@@ -0,0 +1,8 @@
+package netty.protocol;
+
+/**
+ * @author zcl
+ * @date 2022/1/10 17:57
+ */
+public class MessageCodec {
+}
