04a0c9a16d66cfc633d1f885bfca749d622f1332
zcl <820861340@qq.com>
2022-01-11 18:00:18 +0800

    1.http protocol design and resolve initial code

--- src/main/java/netty/message/GroupMembersResponseMessage.java
+++ src/main/java/netty/message/GroupMembersResponseMessage.java
@@ -17,6 +17,6 @@ public class GroupMembersResponseMessage extends Message {
 
     @Override
     public int getMessageType() {
-        return getMessageType();
+        return GroupMembersResponseMessage;
     }
 }
--- src/main/java/netty/protocol/MessageCodec.java
+++ src/main/java/netty/protocol/MessageCodec.java
@@ -9,21 +9,5 @@ import java.util.List;
  * @author zcl
  * @date 2022/1/10 17:57
  */
-public class MessageCodec extends ByteToMessageCodec<Message> {
-
-    @Override
-    protected void encode(ChannelHandlerContext ctx, Message msg, ByteBuf out) throws Exception {
-        out.writeBytes(new byte[]{1,2,3,4});
-        out.writeByte(1);
-        //序列化方式 0 jdk 1 json
-        out.writeByte(0);
-        //指令类型
-
-
-    }
-
-    @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-
-    }
+public class MessageCodec {
 }
