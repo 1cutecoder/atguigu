f1e82e4a329cb94a135c8a99d2d2ac914ad99b75
zcl <820861340@qq.com>
2022-01-12 10:41:41 +0800

    1.http protocol design and resolve 2.stick package and half package test

--- src/main/java/netty/message/LoginRequestMessage.java
+++ src/main/java/netty/message/LoginRequestMessage.java
@@ -8,8 +8,15 @@ import lombok.ToString;
 public class LoginRequestMessage extends Message {
     private String username;
     private String password;
+    private String nickName;
 
     public LoginRequestMessage() {
+    }
+
+    public LoginRequestMessage(String username, String password, String nickName) {
+        this.username = username;
+        this.password = password;
+        this.nickName = nickName;
     }
 
     public LoginRequestMessage(String username, String password) {
