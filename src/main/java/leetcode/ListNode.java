974d64f56f38e28ec4700ee0b96d0b8ee38a1346
zcl <820861340@qq.com>
2022-01-13 14:40:32 +0800

    1.list node initial

---
+++ src/main/java/leetcode/ListNode.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * @author zcl
+ * @date 2022/1/13 14:16
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
