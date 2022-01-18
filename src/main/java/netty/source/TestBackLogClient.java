c8117994accf2bbea92f68d26c2427ba25a05161
zcl <820861340@qq.com>
2022-01-18 16:19:15 +0800

    netty 参数调优 backlog

---
+++ src/main/java/netty/source/TestBackLogClient.java
@@ -0,0 +1,30 @@
+package netty.source;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * @author zcl
+ * @date 2022/1/18 14:52
+ */
+public class TestBackLogClient {
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        ChannelFuture future = new Bootstrap()
+                .group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new LoggingHandler());
+                    }
+                })
+                .connect("127.0.0.1", 8080).sync();
+        future.channel().closeFuture().sync();
+    }
+}
