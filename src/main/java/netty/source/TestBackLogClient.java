a416045f2235536f76c126072621a389335f0279
zcl <820861340@qq.com>
2022-01-18 17:44:49 +0800

    netty 参数调优 bytebuf

--- src/main/java/netty/source/TestBackLogClient.java
+++ src/main/java/netty/source/TestBackLogClient.java
@@ -1,17 +1,18 @@
 package netty.source;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author zcl
  * @date 2022/1/18 14:52
  */
+@Slf4j
 public class TestBackLogClient {
     public static void main(String[] args) throws InterruptedException {
         NioEventLoopGroup group = new NioEventLoopGroup();
@@ -22,6 +23,14 @@ public class TestBackLogClient {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
                         ch.pipeline().addLast(new LoggingHandler());
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                ByteBuf buf = ctx.alloc().buffer();buf.writeBytes("hello!".getBytes());
+                                log.debug("client buf:{}",buf);
+                                ctx.writeAndFlush(buf);
+                            }
+                        });
                     }
                 })
                 .connect("127.0.0.1", 8080).sync();
