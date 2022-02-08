c12475bfc434a0113e84c96f01a8db659b753f34
zcl <820861340@qq.com>
2022-02-08 16:25:39 +0800

    leetcode dfs

---
+++ src/main/java/leetcode/class06/Code02_DFS.java
@@ -0,0 +1,32 @@
+package leetcode.class06;
+
+import java.util.HashSet;
+import java.util.Stack;
+
+/**
+ * @author zcl
+ * @date 2022/2/8 16:20
+ */
+public class Code02_DFS {
+    public static void dfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        HashSet<Node> set = new HashSet<>();
+        stack.push(node);
+        set.add(node);
+        System.out.println(node.value);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)){
+                    stack.push(next);
+                    set.add(next);
+                    System.out.println(next.value);
+                    break;
+                }
+            }
+        }
+    }
+}
