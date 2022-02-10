3d49a648ace263cfebf9346d57afda74269ed50d
zcl <820861340@qq.com>
2022-02-10 17:33:41 +0800

    leetcode 判断是否是平衡二叉树

---
+++ src/main/java/leetcode/class05/Code06_IsBalancedBinaryTree.java
@@ -0,0 +1,17 @@
+package leetcode.class05;
+
+/**
+ * @author zcl
+ * @date 2022/2/10 17:16
+ */
+public class Code06_IsBalancedBinaryTree {
+
+    public static class ReturnType{
+        public boolean isBalanced;
+        public int height;
+    }
+    public static boolean process(Node head) {
+
+        return true;
+    }
+}
