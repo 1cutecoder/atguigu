d9ebed20b142b5dbe90ea59500e0d564c3aa1ae8
zcl <820861340@qq.com>
2022-03-04 14:55:54 +0800

    data comparator

---
+++ src/main/java/DataComparator.java
@@ -0,0 +1,109 @@
+import java.util.Arrays;
+
+/**
+ * @author zcl
+ * @date 2022/3/4 14:54
+ */
+public class DataComparator {
+    public static void insertionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                swap(arr, j, j + 1);
+
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+    public static void main(String[] args) {
+        //generateRandomARRAY
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            insertionSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        insertionSort(arr);
+        printArray(arr);
+    }
+
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        //int range [0,maxSize]
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    /**
+     * for test
+     */
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    /**
+     * for test
+     */
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if (arr1 == null && arr2 != null) {
+            return false;
+        }
+        if (arr1 != null && arr2 == null) {
+            return false;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i : arr) {
+            sb.append(i);
+            sb.append(",");
+        }
+        String s = sb.substring(0, sb.length() - 1);
+        s += "]";
+        System.out.println(s);
+    }
+}
