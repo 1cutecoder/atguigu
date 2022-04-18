f0a448f0248249c45be1bbc8fc2abc6c0c329b50
zcl <820861340@qq.com>
2022-04-18 14:05:41 +0800

    java concurrent classic demo thread  state

---
+++ src/main/java/juc/classicdemo/SleepUtils.java
@@ -0,0 +1,16 @@
+package juc.classicdemo;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zcl
+ * @date 2022/4/14 16:36
+ */
+public class SleepUtils {
+    public static final void second(long seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+        }
+    }
+}
