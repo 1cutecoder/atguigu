99c5ea25ddccd36853f180e67681fe2c30e364bf
zcl <820861340@qq.com>
2022-04-28 09:46:43 +0800

    DeadLock

--- src/main/java/juc/DeadLockDemo.java
+++ src/main/java/juc/DeadLockDemo.java
@@ -2,7 +2,7 @@ package juc;
 
 import java.util.concurrent.TimeUnit;
 
-class HoldLockThread implements Runnable {
+class HoldLockThread implements  Runnable {
     private String lockA;
     private String lockB;
 
@@ -14,31 +14,26 @@ class HoldLockThread implements Runnable {
     @Override
     public void run() {
         synchronized (lockA) {
-            System.out.println(Thread.currentThread().getName() + "\t �Լ�����" + lockA + "\t ���Ի��" + lockB);
-            try {
-                TimeUnit.SECONDS.sleep(2);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            System.out.println(Thread.currentThread().getName() + "\t 自己持有"+lockA+"\t 尝试获得"+lockB);
+            try {TimeUnit.SECONDS.sleep(2);} catch (InterruptedException e) {e.printStackTrace();}
             synchronized (lockB) {
-                System.out.println(Thread.currentThread().getName() + "\t �Լ�����" + lockB + "\t ���Ի��" + lockA);
+                System.out.println(Thread.currentThread().getName() + "\t 自己持有"+lockB+"\t 尝试获得"+lockA);
             }
         }
     }
 }
-
 /**
  * @author zcl
  * @date 2021/9/7 15:00
  */
 public class DeadLockDemo {
     public static void main(String[] args) {
-        String lockA = "lockA";
-        String lockB = "lockB";
-        new Thread(new HoldLockThread(lockA, lockB), "ThreadAAA").start();
-        new Thread(new HoldLockThread(lockB, lockA), "ThreadBBB").start();
+        String  lockA="lockA";
+        String  lockB="lockB";
+        new Thread(new HoldLockThread(lockA,lockB),"ThreadAAA").start();
+        new Thread(new HoldLockThread(lockB,lockA),"ThreadBBB").start();
         /**
-         * windows�µ� jps=ps ef...
+         * windows下的 jps=ps ef...
          */
 
     }
