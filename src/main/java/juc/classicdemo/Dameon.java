6e8094a5158f3dafe4cb6a14915525ea8d162a4b
zcl <820861340@qq.com>
2022-04-29 14:37:39 +0800

    daemon thread finally block will not be invoked

---
+++ src/main/java/juc/classicdemo/Dameon.java
@@ -0,0 +1,19 @@
+package juc.classicdemo;
+
+/**
+ * @author zcl
+ * @date 2022/4/29 14:32
+ */
+public class Dameon {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            try {
+                SleepUtils.second(10);
+            } finally {
+                System.out.println("DaemonThread finally run.");
+            }
+        }, "daemonThread");
+        thread.setDaemon(true);
+        thread.start();
+    }
+}
