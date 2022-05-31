dd5902c011ef589a9241d58d3f25518d9aea70ca
zcl <820861340@qq.com>
2022-05-31 16:58:58 +0800

    candy genericity

---
+++ src/main/java/jvm/candy/Candy3.java
@@ -0,0 +1,37 @@
+package jvm.candy;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author zcl
+ * @date 2022/5/31 16:44
+ */
+public class Candy3 {
+    public static void main(String[] args) throws NoSuchMethodException {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        Integer x = list.get(0);
+        Method test = Candy3.class.getMethod("test", List.class, Map.class);
+        Type[] types = test.getGenericParameterTypes();
+        for (Type type : types) {
+            if (type instanceof ParameterizedType) {
+                ParameterizedType parameterizedType = (ParameterizedType) type;
+                System.out.println("原始类型 - " + parameterizedType.getRawType());
+                Type[] arguments = parameterizedType.getActualTypeArguments();
+                for (int i = 0; i < arguments.length; i++) {
+                    System.out.printf("泛型参数[%d] - %s\n", i, arguments[i]);
+                }
+            }
+        }
+    }
+
+    public Set<Integer> test(List<String> list, Map<Integer, Object> map) {
+        return null;
+    }
+}
