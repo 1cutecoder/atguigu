0ab961278dcb5de636a51d7a5f7ce7c98e8eb5f0
coder <820861340@qq.com>
2022-06-21 15:31:53 +0800

    376WiggleMaxLength

---
+++ src/main/java/leetcode/a376WiggleMaxLength.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+/**
+ * 类描述
+ *
+ * @author zcl
+ * @Description TODO
+ * @Date 2022/6/21 14:59
+ */
+public class a376WiggleMaxLength {
+    public int wiggleMaxLength(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int curDiff = 0;
+        int preDiff = 0;
+        int result = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            curDiff = nums[i + 1] - nums[i];
+            if ((curDiff > 0 && preDiff <= 0) || (curDiff < 0 && preDiff >= 0)) {
+                result++;
+                preDiff = curDiff;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        a376WiggleMaxLength solution = new a376WiggleMaxLength();
+        int[] nums = {1,17,5,10,13,15,10,5,16,8};
+        int result = solution.wiggleMaxLength(nums);
+        System.out.println("result = " + result);
+    }
+}
