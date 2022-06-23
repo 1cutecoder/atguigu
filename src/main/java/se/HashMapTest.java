cc1fe1f6a33e43b7b43ea865f6da93ac7ec487d4
coder <820861340@qq.com>
2022-06-23 10:32:41 +0800

    HashMapTest

--- src/main/java/se/HashMapTest.java
+++ src/main/java/se/HashMapTest.java
@@ -3,22 +3,22 @@ package se;
 import java.util.HashMap;
 
 /**
+ * 类描述
+ *
  * @author zcl
- * @date 2022/5/9 10:03
+ * @Description TODO
+ * @Date 2022/5/9 19:32
  */
 public class HashMapTest {
     public static void main(String[] args) {
-        HashMap<Object, Object> map = new HashMap<>(5);
-        map.put(1,1);
-        map.remove(1);
-        String s = " ";
-
-        //int i = s.hashCode();
-        int i = 2 << 10 -2;
-        System.out.println("i = " + i);
-        System.out.println(i & ((2 << 4) - 1));
-        System.out.println("factor = " + (2 << 20 - 1));
-        System.out.println(2 << 3);
-        System.out.println(8 >> 3);
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("刘德华", 53);
+        map.put("柳岩", 35);
+        map.put("张学友", 55);
+        map.put("郭富城", 52);
+        map.put("黎明", 51);
+        map.put("林青霞", 55);
+        map.put("刘德华", 50);
+        map.remove("刘德华");
     }
 }
