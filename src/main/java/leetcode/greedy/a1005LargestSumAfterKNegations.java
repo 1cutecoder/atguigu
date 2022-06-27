97385e3e219b73aee356c379735700fedfe967e2
coder <820861340@qq.com>
2022-06-27 23:35:30 +0800

    greedy algorithm

---
+++ src/main/java/leetcode/greedy/a1005LargestSumAfterKNegations.java
@@ -0,0 +1,39 @@
+package leetcode.greedy;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * 类描述
+ *
+ * @author zcl
+ * @Description TODO
+ * @Date 2022/6/25 11:04
+ */
+public class a1005LargestSumAfterKNegations {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        nums = IntStream.of(nums)
+                .boxed()
+                .sorted((o1, o2) -> Math.abs(o2) - Math.abs(o1))
+                .mapToInt(Integer::intValue).toArray();
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            if (nums[i] <0 && k>0) {
+                nums[i] = -nums[i];
+                k--;
+            }
+        }
+        if (k % 2 == 1) {
+            nums[length - 1] = -nums[length - 1];
+        }
+        return Arrays.stream(nums).sum();
+
+    }
+
+    public static void main(String[] args) {
+        a1005LargestSumAfterKNegations solution = new a1005LargestSumAfterKNegations();
+        int nums[]={4,2,3};
+        int  k = 1;
+        System.out.println(solution.largestSumAfterKNegations(nums, k));
+    }
+}
---
+++ src/main/java/leetcode/greedy/a45Jump.java
@@ -0,0 +1,43 @@
+package leetcode.greedy;
+
+/**
+ * 类描述
+ *
+ * @author zcl
+ * @Description 定一个非负整数数组，你最初位于数组的第一个位置。
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+ * @Date 2022/6/24 11:01
+ */
+public class a45Jump {
+    public int jump(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+        //当前覆盖最远距离下标
+        int curDistance = 0;
+        //走的最大步数
+        int count = 0;
+        //下一步覆盖最远距离下标
+        int maxDistance = 0;
+        for (int i = 0; i < nums.length; i++) {
+            maxDistance = Math.max(nums[i] + i, maxDistance);
+            if (maxDistance >= nums.length -1){
+                count++;
+                break;
+            }
+            if (curDistance == i) {
+                curDistance = maxDistance;
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        a45Jump solution = new a45Jump();
+        int[] nums = {2, 1, 1, 1, 4};
+        int jump = solution.jump(nums);
+        System.out.println("jump = " + jump);
+    }
+}
