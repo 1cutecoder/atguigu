9cbf70fdb04506f556132a0690db99a7ce2bf7ff
coder <820861340@qq.com>
2022-07-20 21:54:01 +0800

    a216CombinationSum

--- src/main/java/leetcode/backtracking/a216CombinationSum.java
+++ src/main/java/leetcode/backtracking/a216CombinationSum.java
@@ -8,8 +8,16 @@ import java.util.List;
  * 类描述
  *
  * @author zcl
- * @Description TODO
+ * @Description 找出所有相加之和为n 的k个数的组合，且满足下列条件：
+ * 只使用数字1到9
+ * 每个数字最多使用一次
+ * 返回 所有可能的有效组合的列表 。该列表不能包含相同的组合两次，组合可以以任何顺序返回
  * @Date 2022/6/21 13:45
+ * 输入: k = 3, n = 7
+ * 输出: [[1,2,4]]
+ * 解释:
+ * 1 + 2 + 4 = 7
+ * 没有其他符合的组合了。
  */
 public class a216CombinationSum {
     List<List<Integer>> result = new ArrayList<>();
