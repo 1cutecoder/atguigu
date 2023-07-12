7653d5ffff84ca7fc376c6cf0812912c30eb1639
coder <820861340@qq.com>
2023-07-12 14:13:12 +0800

    每日温度

--- src/main/java/leetcode/DailyTemperatures739a.java
+++ src/main/java/leetcode/DailyTemperatures739a.java
@@ -11,6 +11,8 @@ import java.util.LinkedList;
  * 返回一个数组 answer ， * 其中 answer[i] 是指对于第 i 天，
  * 下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置
  * 用 0 来代替。
+ * 输入: temperatures = [73,74,75,71,69,72,76,73]
+ * 输出: [1,1,4,2,1,1,0,0]
  * @Date 2023/7/12 0:35
  */
 public class DailyTemperatures739a {
@@ -34,11 +36,17 @@ public class DailyTemperatures739a {
                 }
                 stack.push(i);
             }
-            return null;
+            return res;
         }
     }
 
     public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arrays = new int[]{73,74,75,71,69,72,76,73};
+        int[] ints = solution.dailyTemperatures(arrays);
+        for (int anInt : ints) {
+            System.out.print(anInt +"\t");
+        }
 
     }
 }
