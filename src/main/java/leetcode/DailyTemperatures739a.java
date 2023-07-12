46bfda5ab59ae2b3d3422df4555bae235631f2a8
coder <820861340@qq.com>
2023-07-12 13:55:53 +0800

    每日温度

---
+++ src/main/java/leetcode/DailyTemperatures739a.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 类描述
+ *
+ * @author zcl
+ * @Description 给定一个整数数组 temperatures ，表示每天的温度，
+ * 返回一个数组 answer ， * 其中 answer[i] 是指对于第 i 天，
+ * 下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置
+ * 用 0 来代替。
+ * @Date 2023/7/12 0:35
+ */
+public class DailyTemperatures739a {
+    static class Solution {
+        /**
+         * 如果当前遍历的元素 大于栈顶元素，表示 栈顶元素的 右边的最大的元素就是 当前遍历的元素，
+         * 所以弹出 栈顶元素，并记录
+         * 如果栈不空的话，还要考虑新的栈顶与当前元素的大小关系
+         * 否则的话，可以直接入栈。
+         * 注意，单调栈里 加入的元素是 下标。
+         */
+        public int[] dailyTemperatures(int[] temperatures) {
+            int lens = temperatures.length;
+            int[] res = new int[lens];
+            Deque<Integer> stack = new LinkedList<>();
+            stack.push(0);
+            for (int i = 0; i < lens; i++) {
+                while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                    res[stack.peek()] = i - stack.peek();
+                    stack.pop();
+                }
+                stack.push(i);
+            }
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
